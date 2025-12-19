eca404e4ff7b3b98bc850ca6ad0e471a6e271eec
Divyansh Bhardwaj <divyanshb22@gmail.com>
2025-12-19 23:54:26 +0530

    feat(atom): refine Button component exports
    - Update barrel exports with improved typing

--- src/components/atoms/Button/index.ts
+++ src/components/atoms/Button/index.ts
@@ -1,2 +1,10 @@
 export { Button } from './Button';
-export type { ButtonProps, ButtonSize, ButtonVariant, ButtonStyle, ButtonAs, ButtonAsButtonProps, ButtonAsAnchorProps } from './ButtonProps';
+export type {
+	ButtonProps,
+	ButtonSize,
+	ButtonVariant,
+	ButtonStyle,
+	ButtonAs,
+	ButtonAsButtonProps,
+	ButtonAsAnchorProps
+} from './ButtonProps';
